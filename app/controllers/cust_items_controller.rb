893af6028ef918ceef449764390451519232da33
Steve De Jongh <de.jongh.steve@gmail.com>
2024-04-03 11:34:30 -0700

    Customer Profile layout improvements

--- app/controllers/cust_items_controller.rb
+++ app/controllers/cust_items_controller.rb
@@ -22,10 +22,11 @@ class CustItemsController < ApplicationController
   # POST /cust_items or /cust_items.json
   def create
     @cust_item = CustItem.new(cust_item_params)
+    @cust_item.customer_id = params[:form][:customer_id].to_i
 
     respond_to do |format|
       if @cust_item.save
-        format.html { redirect_to cust_item_url(@cust_item), notice: "Cust item was successfully created." }
+        format.html { redirect_to customer_url(@cust_item.customer_id)}
         format.json { render :show, status: :created, location: @cust_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -65,6 +66,6 @@ class CustItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def cust_item_params
-      params.fetch(:cust_item, {})
+      params.require(:cust_item).permit(:make, :model, :color, :size, :customer_id)
     end
 end
